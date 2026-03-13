4a7a719e963bdee162fa2e61391629f9489e22ad
Subagent <pengshanhui@gmail.com>
2026-03-13 21:20:59 +0800

    style: increase header Yenturi logo size by 20%

--- components/Header.tsx
+++ components/Header.tsx
@@ -57,7 +57,7 @@ export default function Header({ locale }: HeaderProps) {
           >
             <span
               className="font-serif font-semibold text-navy-900 tracking-tight"
-              style={{ fontSize: '1.0625rem', letterSpacing: '-0.01em' }}
+              style={{ fontSize: '1.275rem', letterSpacing: '-0.01em' }}
             >
               Yenturi
             </span>
